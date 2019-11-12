fd8c59f85ddefa1e8eeb9f67cd30a6b4da374786
allure <lambsliu@qq.com>
2019-11-13 00:48:54 +0800

    page one 数据库插入功能完成

---
+++ code/db_test.py
@@ -0,0 +1,33 @@
+import configparser
+import time
+from datetime import datetime
+import pymysql
+
+
+def dbConnect():
+    config = configparser.ConfigParser()
+    config.read('/Users/schrodinger/njorg/config/mysql', encoding='utf-8')
+    ip = config.get('mysql-db', 'ip')
+    db_name = config.get('mysql-db', 'db_name')
+    username = config.get('mysql-db', 'username')
+    password = config.get('mysql-db', 'password')
+    conn = pymysql.connect('122.51.219.104', 'root', 'root2019', 'njorg')
+    cur = conn.cursor()
+    sql = "insert into njorg(article_link,article_title,article_content,article_author,article_annex_link,article_time,created_time) values(%s,%s,%s,%s,%s,%s,%s)"
+    article_link = 'article_link'
+    article_title = 'article_title'
+    article_content = 'article_content'
+    article_author = 'article_author'
+    article_annex_link = 'article_annex_link'
+    article_time = '2019-11-12'
+    created_time =time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    par = (
+    article_link, article_title, article_content, article_author, article_annex_link, article_time, created_time)
+    conn.ping(reconnect=True)
+    cur.execute(sql, par)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    dbConnect()
--- code/njorg.py
+++ code/njorg.py
@@ -1,60 +1,76 @@
 import configparser
+import time
 import urllib.request
+from datetime import datetime
+
 import pymysql
 from bs4 import BeautifulSoup
-from datetime import datetime
-import time
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
 }
-host = 'http://njna.ngorg.gov.cn/cxrc/cxrczc/zcjj/'
+host = 'http://njna.nanjing.gov.cn/cxrc/cxrczc/zcjj/'
+
+"""
+    article_time 由web获取的text进行转换
+    created_time 由本地时间戳进行格式化
+"""
 
 
-def dbConnect(*args):
+def dbConnect():
     config = configparser.ConfigParser()
-    config.read('/Users/schrodinger/PycharmProjects/ngorg/config/mysql', encoding='utf-8')
+    config.read('/Users/schrodinger/njorg/config/mysql', encoding='utf-8')
     ip = config.get('mysql-db', 'ip')
     db_name = config.get('mysql-db', 'db_name')
     username = config.get('mysql-db', 'username')
     password = config.get('mysql-db', 'password')
-
-    conn = pymysql.connect(ip, username, password, db_name).cursor()
-    sql = 'inset into njorg(article_link,article_title,article_content,article_author,article_annex_link,article_time,created_time) values(%$,%s,%s,%s,%s,%d,%d)'
-    article_link = ''
-    article_title = ''
-    article_content = ''
-    article_author =''
-    article_author = ''
-    article_annex_link = ''
-
-
-
+    print(ip, db_name, username, password)
+    conn = pymysql.connect('122.51.219.104', 'root', 'root2019', 'njorg')
+    cur = conn.cursor()
+    sql = "insert into njorg(article_link,article_title,article_content,article_author,article_annex_link,article_time,created_time) values(%s,%s,%s,%s,%s,%s,%s)"
+    article_link = 'article_link'
+    article_title = 'article_title'
+    article_content = 'article_content'
+    article_author = 'article_author'
+    article_annex_link = 'article_annex_link'
+    article_page_time = '2019-11-12'
+    article_time = datetime.strptime(article_page_time, '%Y-%m-%d')
+    created_time =datetime.strptime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),"%Y-%m-%d %H:%M:%S")
+    # created_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    par = (
+    article_link, article_title, article_content, article_author, article_annex_link, article_time, created_time)
+    conn.ping(reconnect=True)
+    cur.execute(sql, par)
+    conn.commit()
+    conn.close()
 
 def getHref():
-    res = urllib.request.urlopen('http://njna.ngorg.gov.cn/cxrc/cxrczc/zcjj').read().decode('utf-8')
+    res = urllib.request.urlopen('http://njna.nanjing.gov.cn/cxrc/cxrczc/zcjj/').read().decode('utf-8')
     home_page = BeautifulSoup(res, 'html.parser')
     # 通过 attrs 查看 class 名称为 dpgl_con的标签
     li = home_page.find(attrs={'class': 'dpgl_con'})
     # 对于还是tag的元素标签可以再次使用 find()||find_all():target 标签名称查找
     paths = li.find_all(target="_blank")
-    # hrefs = []
     for one in paths:
         href_orign = one['href']
         # python分割字符串 str[2:] 其实变相当作数组进行处理
         href_ex = href_orign[2:]
         url = host + href_ex
+        print(url)
         article(url)
 
 
 def article(url):
-    if url[-4:] != '.pdf':
+    if url[-4:] == '.pdf':
+        response = urllib.request.urlopen(url)
+        file = open("document.pdf", 'w')
+        file.write(response.read())
+        file.close()
+    elif url[-4:] == '.html':
         res = urllib.request.urlopen(url).read().decode('utf-8')
         page = BeautifulSoup(res, 'html.parser')
-
     else:
         pass
 
-
 if __name__ == '__main__':
-    dbConnect()
+    getHref()
